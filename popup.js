f2ad561fd95e5add1fd82cea87c337cc2e1d222e
DeveloperAvik <98541847+DeveloperAvik@users.noreply.github.com>
2024-01-24 16:30:21 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,25 @@
+if (document.querySelector(".popup")) {
+
+    const button = document.querySelector(".button");
+    const circle = document.querySelector(".circle");
+
+    let buttonOn = false;
+
+    button.addEventListener("click", () => {
+        if (!buttonOn) {
+            buttonOn = true;
+            circle.style.animation = "moveCricleRight 1s forwards";
+            button.style.animation = "transformToYellow 1s forwards";
+            chrome.tabs.executeScript({
+                file: "appOn.js"
+            })
+        } else {
+            buttonOn = false;
+            circle.style.animation = "moveCricleLeft 1s forwards";
+            button.style.animation = "transformToBlue 1s forwards";
+            chrome.tabs.executeScript({
+                file: "appOff.js"
+            })
+        }
+    })
+}
